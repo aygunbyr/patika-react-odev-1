a958464ef015df87e6d6a40043bc1a38237365dd
Aygün Bayır <turkoaygun@gmail.com>
2023-03-28 22:49:05 +0300

    Add comments

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 import getData from "./getData.js";
 
+// IIFE that gets user data and prints to console
 (async function () {
+  // get data from module
   const data = await getData(1);
+
+  // print data to console
   console.log(data);
 })();
--- getData.js
+++ getData.js
@@ -1,15 +1,27 @@
 import axios from "axios";
 
+/**
+ * Function that gets user data
+ * @param {*} id User id
+ * @returns User data
+ */
 async function getData(id) {
+  // fetch user data using axios
   const { data: user } = await axios(
     `https://jsonplaceholder.typicode.com/users/${id}`
   );
+
+  // fetch post data using axios
   const { data: posts } = await axios(
     `https://jsonplaceholder.typicode.com/posts?userId=${id}`
   );
+
+  // add posts data to user data
   user.posts = posts;
 
+  // return user data
   return user;
 }
 
+// export getData function
 export default getData;
